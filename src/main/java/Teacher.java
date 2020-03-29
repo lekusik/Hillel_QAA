c82b0240720fb5ad8d9eaf74334a76706785105c
Leka Ananieva <lekusik+git@gmail.com>
2020-03-29 23:10:51 +0300

    Lesson 7 HomeWork

---
+++ src/main/java/Teacher.java
@@ -0,0 +1,23 @@
+//  Домашнее задание 7
+//        Добавлено: 27.03.2020 21:58
+//        Ознакомление с классами
+//        1.  Создать клас Ученик с полями, имя, фамилия, возраст,
+//            и через this задать ему поля и вывести в консоль эти поля.
+//        2.  Создать клас Учитель с полями, имя, фамилия, возраст,
+//            и через геттеры и сеттеры задать поля и вывести в консоль эти поля.
+
+public class Teacher {
+    String FName;   //  имя
+    String LName;   //  фамилия
+    int age;        //  возрвст
+
+//  геттеры и сеттеры для заполения полей класса
+    void setFName(String FName) { this.FName = FName; }
+    String getFName() { return FName; }
+
+    void setLName(String LName) { this.LName = LName; }
+    String getLName() { return LName; }
+
+    void setAge(int age) { this.age = age; }
+    int getAge() { return age; }
+}
